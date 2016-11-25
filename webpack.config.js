796565f94ec361310ed9795da6f1d0d7447b1dff
Marius Lazar <marius.lazar@luminoslabs.com>
2016-11-25 18:21:14 +0200

    30% completed

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './app/js/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: "index-bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: [/\.js$/],
+                exclude: /node_modules/,
+                loader: "babel",
+                query:{
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    }
+}
